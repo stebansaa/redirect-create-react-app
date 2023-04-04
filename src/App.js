cbd20cee8791afdb273d181566067f6da560024f
Esteban Saá <steban@gmail.com>
2023-04-03 20:24:47 -0500

    simpler

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import { useEffect } from 'react';
+import './App.css';
 
 function App() {
-  useEffect(() => {
+  const handleClick = () => {
     window.location.href = 'http://frogge.finance';
-  }, []);
+  };
 
-  return null;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <button onClick={handleClick}>Go to Frogge.finance</button>
+      </header>
+    </div>
+  );
 }
 
 export default App;
